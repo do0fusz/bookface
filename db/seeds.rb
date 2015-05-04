d5feb8ca2fc1469e11db416b875cbce29eb681a8
do0fusz <do0fusz@hotmail.com>
2015-05-04 13:35:45 +0200

    added database seed manually

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "do0fusz", email: "do0fusz@hotmail.com", password: "cupertino2010", password_confirmation: "cupertino2010")
+User.create(username: "1do0fusz", email: "1do0fusz@hotmail.com", password: "cupertino2010", password_confirmation: "cupertino2010")
+User.create(username: "2do0fusz", email: "2do0fusz@hotmail.com", password: "cupertino2010", password_confirmation: "cupertino2010")
+User.create(username: "3do0fusz", email: "3do0fusz@hotmail.com", password: "cupertino2010", password_confirmation: "cupertino2010")
+User.create(username: "4do0fusz", email: "4do0fusz@hotmail.com", password: "cupertino2010", password_confirmation: "cupertino2010")
+User.create(username: "5do0fusz", email: "5do0fusz@hotmail.com", password: "cupertino2010", password_confirmation: "cupertino2010")
+
+
+p "Test users created"
