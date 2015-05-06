d72eb840bc18c10751fc2ccf889992a8469d759f
do0fusz <do0fusz@hotmail.com>
2015-05-06 15:05:37 +0200

    friendships model

---
+++ app/models/friendship.rb
@@ -0,0 +1,18 @@
+class Friendship < ActiveRecord::Base
+  
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
+
+  def cancel_friendship
+    self.friendships.destroy
+  end
+
+  def delete_friendship
+    self.destroy 
+  end
+
+  def accept_friendship
+    self.update_attributes(state: "accepted ")
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ActiveRecord::Base
   validates_presence_of :username 
   validates_uniqueness_of :username
 
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendships", foreign_key: "User", dependent: :destroy
+
+  def request_friendship(user2)
+    self.friendships.create(friend_id: user2.id)    
+  end
 end
